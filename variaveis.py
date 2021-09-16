f95864d98b3b32349aa23f2f644d14da4f677d6a
Kate Keiroz <katekeiroz@gmail.com>
2021-09-16 09:23:50 +0100

    initial commit

---
+++ variaveis.py
@@ -0,0 +1,18 @@
+x = 10
+y = 2.3
+z = "Kate"
+print(type(x))
+print(type(y))
+print(type(z))
+
+# para converter o tipo de variavel :
+
+variavel_float = 5.2
+print(variavel_float)
+variavel_int = int(variavel_float)
+print(variavel_int)
+
+#  forma de colocar o marcador
+
+nome = input("Digite seu nome")
+print("Seu nome é {}".format(nome))
